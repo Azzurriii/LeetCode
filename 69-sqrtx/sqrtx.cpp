7ae1114cc5296da758c35c707b87af2450ffc1f5
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-11-11 10:02:08 +0700

    Time: 2 ms (49.24%) | Memory: 6.5 MB (14.01%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,27 @@
 class Solution {
+private:
+    int sqrtHelper(int x, int left, int right) {
+        if (left > right) {
+            return right;
+        }
+        
+        int mid = left + (right - left) / 2;
+
+        if (mid <= x / mid) {
+            // If mid is less than or equal to the square root of x, search in the right half.
+            return sqrtHelper(x, mid + 1, right);
+        } else {
+            // If mid is greater than the square root of x, search in the left half.
+            return sqrtHelper(x, left, mid - 1);
+        }
+    }
+
 public:
     int mySqrt(int x) {
-        return (int)sqrt(x);
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        return sqrtHelper(x, 1, x);
     }
 };
