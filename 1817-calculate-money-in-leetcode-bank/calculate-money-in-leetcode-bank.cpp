e48c087a25f483c16228ee575e59c83e8213bf6d
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-06 09:20:45 +0700

    Time: 2 ms (49.74%) | Memory: 6.3 MB (35.53%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int numOfFullWeek = n / 7;
-        int firstFullWeek = 28;
-        int lastFullWeek = 28 + (numOfFullWeek - 1) * 7;
-        int moneyOfFullWeek = numOfFullWeek * (firstFullWeek + lastFullWeek) / 2;
-        
-        int monday = 1 + numOfFullWeek;
-        int finalWeek = 0;
-        for (int dayLeft = 0; dayLeft < n % 7; dayLeft++) {
-            finalWeek += monday + dayLeft;
-        }
-        
-        return moneyOfFullWeek + finalWeek;
+        return (n / 7) * 28 + ((n/7 * (n/7 - 1))/2) * 7 + ((n % 7 - 1) * (n % 7)) / 2 + (n % 7) * (n / 7 + 1); 
     }
 };
