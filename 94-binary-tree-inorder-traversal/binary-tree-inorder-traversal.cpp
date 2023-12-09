f3abc0018b2c7a0db6652dbd7450d3eb63568c1f
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-09 15:07:55 +0700

    Time: 4 ms (33.83%) | Memory: 8.8 MB (31.48%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,17 +1,43 @@
-class Solution {
-private:
-    void LNR(TreeNode* root, vector<int>& res)
-    {
-        if(root){
-            LNR(root->left, res);
-            res.push_back(root->val);
-            LNR(root->right, res);
-        }
-    }
+//#Recursion
+// class Solution {
+// private:
+//     void LNR(TreeNode* root, vector<int>& res)
+//     {
+//         if(root){
+//             LNR(root->left, res);
+//             res.push_back(root->val);
+//             LNR(root->right, res);
+//         }
+//     }
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         LNR(root, res);
+//         return res;
+//     }
+// };
+
+//#Daily 9/12: No Recursion, Iterative.
+class Solution{
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> res;
-        LNR(root, res);
-        return res;
+    vector<int> inorderTraversal(TreeNode* root){
+        vector<int> result;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        
+        while (curr != nullptr || !st.empty()) {
+            while (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            
+            curr = st.top();
+            st.pop();
+            result.push_back(curr->val);
+            
+            curr = curr->right;
+        }
+        
+        return result;
     }
 };
