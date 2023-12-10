80bc32bc838d9f07a4290e7a1e6fc06b1e274b9d
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-10 09:36:08 +0700

    Time: 6 ms (89.39%) | Memory: 11 MB (25.39%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.cpp
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, 0));
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                ans[j][i] = matrix[i][j];
+        vector<vector<int>> res(matrix[0].size(), vector<int>(matrix.size()));
+        
+        for (int i=0;i<matrix.size();i++) {
+            for (int j=0;j<matrix[0].size();j++) {
+                res[j][i]=matrix[i][j];
             }
         }
-        return ans;
+        return res;
     }
 };
