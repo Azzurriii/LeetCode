e8b13d6370be508f71818a50a8808fbaa8facca2
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-10 16:36:14 +0700

    Time: 15 ms (19.47%) | Memory: 7.6 MB (78.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,9 +5,6 @@ public:
             return false;
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        if(s == t)
-            return true;
-        else
-            return false;
+        return (s == t);
     }
 };
