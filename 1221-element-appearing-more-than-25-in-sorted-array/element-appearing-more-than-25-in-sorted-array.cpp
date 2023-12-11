60ac4447acbab67bccad712ed40b29c47e94cafe
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-11 16:11:46 +0700

    Time: 6 ms (92.41%) | Memory: 12.7 MB (92.26%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,41 +1,15 @@
-// class Solution {
-// public:
-//     //Sliding windows
-//     int findSpecialInteger(vector<int>& arr) {
-//         // int windowSize = arr.size() / 4;
-
-//         // for (int i = 0; i < arr.size() - windowSize; i++) {
-//         //     if (arr[i] == arr[i + windowSize]) {
-//         //         return arr[i];
-//         //     }
-//         // }
-        
-//         // return 0;
-//         int l = arr.size();
-//         int p = l >> 2;
-//         for (int i = 0; i < l; i += p) {
-//             if (upper_bound(arr.begin(), arr.end(), arr[i]) - lower_bound(arr.begin(), arr.end(), arr[i]) > l >> 2) {
-//                 return arr[i];
-//             }
-//         }
-//         return 0;
-//     }
-// };
 class Solution {
 public:
+    //Sliding windows
     int findSpecialInteger(vector<int>& arr) {
-        int n = arr.size();
-        int count = 1;
-        int p = arr[0];
-        int rate = n / 4;
-        for (int i = 1; i < n; i++)
-        {
-            if (arr[i] == p) count++;
-            else count = 1;
+        int windowSize = arr.size() / 4;
 
-            if (count > rate) return p;
-            p = arr[i];
+        for (int i = 0; i < arr.size() - windowSize; ++i) {
+            if (arr[i] == arr[i + windowSize]) {
+                return arr[i];
+            }
         }
-        return count;
+        
+        return 0;
     }
 };
