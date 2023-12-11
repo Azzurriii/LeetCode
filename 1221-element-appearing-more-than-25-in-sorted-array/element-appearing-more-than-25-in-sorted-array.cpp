cb14f78e36ba2815f5c9297adca3e8f6aceb4371
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-11 16:06:58 +0700

    Time: 8 ms (78.76%) | Memory: 12.9 MB (57.06%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -2,14 +2,23 @@ class Solution {
 public:
     //Sliding windows
     int findSpecialInteger(vector<int>& arr) {
-        int windowSize = arr.size() / 4;
+        // int windowSize = arr.size() / 4;
 
-        for (int i = 0; i < arr.size() - windowSize; i++) {
-            if (arr[i] == arr[i + windowSize]) {
+        // for (int i = 0; i < arr.size() - windowSize; i++) {
+        //     if (arr[i] == arr[i + windowSize]) {
+        //         return arr[i];
+        //     }
+        // }
+        
+        // return 0;
+        int l = arr.size();
+        int p = l / 4 + (l % 4 != 0);
+        for (int i = 0; i < l; i += p) {
+            int res = upper_bound(arr.begin(), arr.end(), arr[i]) - lower_bound(arr.begin(), arr.end(), arr[i]);
+            if (res > l / 4) {
                 return arr[i];
             }
         }
-        
         return 0;
     }
 };
