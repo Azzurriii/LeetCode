6b9fca24c59021894c19a45639204c8c9ccf8ce3
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-12 10:24:45 +0700

    Time: 3 ms (82.74%) | Memory: 10.4 MB (25.23%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
     int maxProduct(vector<int>& nums) {
         auto it = std::max_element(nums.begin(), nums.end());
         int x=*it-1;
-        nums.erase(it);  // Fix: use iterator directly without '+'
+        nums.erase(it);  
         auto yt = std::max_element(nums.begin(), nums.end());
         cout<<*yt<<endl;
         return (x) * (*yt - 1);
