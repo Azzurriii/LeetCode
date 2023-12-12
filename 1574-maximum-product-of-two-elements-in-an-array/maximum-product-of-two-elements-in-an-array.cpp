e0f162acac87809a28a1f43e103d186ca4980fc5
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-12 10:27:42 +0700

    Time: 0 ms (100.00%) | Memory: 10.3 MB (38.51%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -5,7 +5,6 @@ public:
         int x=*it-1;
         nums.erase(it);  
         auto yt = std::max_element(nums.begin(), nums.end());
-        cout<<*yt<<endl;
         return (x) * (*yt - 1);
     }
 };
