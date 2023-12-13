b5233d204fc72cdec4338c08a4a4324b6df21073
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-13 08:43:47 +0700

    Time: 6 ms (31.60%) | Memory: 7.1 MB (60.33%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -7,6 +7,7 @@ public:
         string s = countAndSay(n - 1);
         string ans = "";
         int count = 1;
+        
         for (int i = 0; i < s.length(); i++)
         {
             if (s[i] == s[i + 1])
