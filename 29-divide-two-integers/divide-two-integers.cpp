1089e31a280e13c823f4e598721406777f1c8818
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-15 14:45:34 +0700

    Time: 0 ms (100.00%) | Memory: 6.5 MB (16.00%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-    int divide(int dividend, int divisor){
-    int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
-    long long int quotient = 0;
-    long long int temp = 0;
-    long long int a = abs(dividend);
-    long long int b = abs(divisor);
-    for(int i = 31; i >= 0; i--){
-        if(temp + (b << i) <= a){
-            temp += b << i;
-            quotient |= 1LL << i;
+    int divide(int dividend, int divisor) {
+         
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
         }
+        long long x = static_cast<long long>(dividend) / divisor;
+        return static_cast<int>(x);
     }
-    quotient *= sign;
-    if(quotient > INT_MAX || quotient < INT_MIN){
-        return INT_MAX;
-    }
-    return quotient;
-}
 };
