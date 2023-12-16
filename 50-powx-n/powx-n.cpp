392978c75cc7f4713392245250b2cec9b9dcb480
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-16 19:03:39 +0700

    Time: 0 ms (100.00%) | Memory: 6.3 MB (95.28%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,24 +1,19 @@
 class Solution {
-private:
-    double power(double x, long long n) {
-        if (n == 0) {
-            return 1;
-        }
-
-        double halfPower = power(x, n / 2);
-        return (n % 2 == 0) ? halfPower * halfPower : x * halfPower * halfPower;
-    }
 
 public:
     double myPow(double x, int n) {
-        if(x == 1.0000000000001 && n == -2147483648) return 0.99979;
-        if (n == INT_MIN) {
-            return (x == 1 || x == -1) ? 1 : 0;
+        if( n==0) 
+            return 1;
+            
+        if(n < 0) {
+            n = abs(n);
+            x = 1/x;
         }
-
-        long long absN = abs(static_cast<long long>(n));
-        double result = power(x, absN);
-
-        return (n < 0) ? 1.0 / result : result;
+        if(n % 2 == 0)
+            return myPow(x * x, n / 2);
+        
+        else
+            return x*myPow(x, n-1);
+        
     }
 };
