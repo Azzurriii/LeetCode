bf3f6888d08f141acaedf90b0e32cbd1894b88d7
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-20 18:49:21 +0700

    Time: 4 ms (93.98%) | Memory: 47.1 MB (29.77%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-   int buyChoco(vector<int>& prices, int money) {
-       nth_element(prices.begin(), prices.begin()+1, prices.end());
-       int left = money - prices[0] - prices[1];
-       return (left >= 0) ? left : money;
-   }
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        return (money - (prices[0]+prices[1]) < 0) ? money : (money-(prices[0]+prices[1]));
+    }
 };
