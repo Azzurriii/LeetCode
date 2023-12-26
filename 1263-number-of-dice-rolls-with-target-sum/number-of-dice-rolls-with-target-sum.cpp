8fc825fbd674b5324decab99b053efbc4ed5705d
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-26 19:17:54 +0700

    Time: 8 ms (95.95%) | Memory: 17.8 MB (6.94%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
-        const int MOD = 1e9 + 7;
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= target; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else if (j < i || i * k < j) {
-                    dp[i][j] = 0;
-                } else {
-                    for (int x = 1; x <= k; x++) {
-                        if (j >= x) {
-                            dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % MOD;
-                        }
-                    }
+        vector<vector<int>>dp(30, vector<int>(1000, 0));
+        //int dp[30][1001] = {0};
+        int mod = 1e9 + 7;
+        for (int j = 1; j <= k && j <= target; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= target; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i][j - 1]) % mod;
+                if (j > k) {
+                    dp[i][j] = (dp[i][j] - dp[i - 1][j - 1 - k] + mod) % mod;
                 }
             }
         }
-
-        return dp[n][target];
+        return dp[n - 1][target];
     }
 };
