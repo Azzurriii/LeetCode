fcc9d977afc24bd2257d3f7cb7b0e1fbc29662c6
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-26 19:21:29 +0700

    Time: 14 ms (89.42%) | Memory: 9 MB (29.50%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(30, vector<int>(1000, 0));
-        //int dp[30][1001] = {0};
         int mod = 1e9 + 7;
-        for (int j = 1; j <= k && j <= target; j++) {
-            dp[0][j] = 1;
-        }
+        vector<vector<int>>dp;
+        dp.resize(n + 1, vector<int>(target + 1, 0));
+        dp[0].resize(min(k + 1, target + 1), 1);
+        fill(dp[0].begin() + 1, dp[0].end(), 1);
         for (int i = 1; i < n; i++) {
             for (int j = 1; j <= target; j++) {
                 dp[i][j] = (dp[i - 1][j - 1] + dp[i][j - 1]) % mod;
