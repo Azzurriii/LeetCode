9982e0ced017a61dcf2795a3152257641343679d
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-27 19:49:33 +0700

    Time: 114 ms (73.03%) | Memory: 95.8 MB (49.48%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -4,14 +4,12 @@ public:
         int time = 0;
         int n = colors.size();
         for(int i = 1; i < n; i++){
-            if(colors[i] == colors[i - 1])
-            {
+            if(colors[i] == colors[i - 1]){
                 time += min(neededTime[i], neededTime[i - 1]); 
                 neededTime[i] = max(neededTime[i], neededTime[i - 1]); 
             }     
         }
         
         return time;
-        
     }
 };
