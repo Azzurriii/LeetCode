151d4a400b90b1e0f408220dc9542e88cf9b1fdf
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2023-12-29 11:10:34 +0700

    Time: 14 ms (95.66%) | Memory: 7.5 MB (88.67%) - LeetSync

---
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        if (jobDifficulty.size() < d) return -1;
+        if (jobDifficulty.size() == d)
+            return accumulate(jobDifficulty.begin(), jobDifficulty.end(), 0);
+
+        int dp[10][300] = {jobDifficulty[0]};
+        int n = jobDifficulty.size();
+        
+        for (int j = 1; j < n; ++j) {
+            dp[0][j] = max(dp[0][j-1], jobDifficulty[j]);
+        }
+
+        for (int i = 1; i < d; ++i) {
+            dp[i][0] = jobDifficulty[0];
+
+            for (int j = 1; j < n; ++j) {
+                int job = jobDifficulty[j];
+                dp[i][j] = dp[i-1][j-1] + job;
+                for (int l = j-1; l >= i; --l) {
+                    job = max(job, jobDifficulty[l]);
+                    dp[i][j] = min(dp[i][j], dp[i-1][l-1] + job);
+                }
+            }
+        }
+        return dp[d-1][n-1];
+    }
+};
