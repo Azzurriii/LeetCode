dbbf2ad4e49d03794f16f9bdff6dae159950021d
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2024-01-06 11:16:09 +0700

    Time: 198 ms (46.54%) | Memory: 84.5 MB (27.12%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int find(int i, vector<vector<int>>& job, vector<int>& startTime, int n, vector<int>& dp) {
+    int solve(int i, vector<vector<int>>& job, vector<int>& startTime, int n, vector<int>& dp) {
         if (i >= n)
             return 0;
         if (dp[i] != -1)
             return dp[i];
         int index = lower_bound(startTime.begin(), startTime.end(), job[i][1]) - startTime.begin();
-        int pick = job[i][2] + find(index, job, startTime, n, dp);
-        int notpick = find(i + 1, job, startTime, n, dp);
+        int pick = job[i][2] + solve(index, job, startTime, n, dp);
+        int notpick = solve(i + 1, job, startTime, n, dp);
         return dp[i] = max(pick, notpick);
     }
 
@@ -23,6 +23,6 @@ public:
         sort(job.begin(), job.end());
         sort(startTime.begin(), startTime.end());
 
-        return find(0, job, startTime, n, dp);
+        return solve(0, job, startTime, n, dp);
     }
 };
