a8fb0082b3bc2d8d2ebbb3ad8b83e5e3c02565d5
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2024-01-13 10:14:58 +0700

    Time: 268 ms (26.43%) | Memory: 14.1 MB (81.43%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,9 +1,4 @@
 class Solution(object):
     def minSteps(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: int
-        """
         return sum((Counter(s) - Counter(t)).values())
         
