bfa5aacc402ce1a4a79948619229f45e98d06c46
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2024-04-24 22:11:24 +0700

    Time: 3 ms (22.77%) | Memory: 7.4 MB (13.08%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+        std::vector<int> tribo(n + 1);
+        tribo[0] = 0;
+        tribo[1] = 1;
+        tribo[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            tribo[i] = tribo[i - 1] + tribo[i - 2] + tribo[i - 3];
+        }
+        return tribo[n];
+    }
+};
