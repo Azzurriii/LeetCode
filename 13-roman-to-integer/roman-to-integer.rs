4af8eecf29287e4312ced2e718e14754024d790a
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2024-08-08 22:17:06 +0700

    Time: 0 ms (100.00%) | Memory: 2.1 MB (80.80%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn roman_to_int(s:String)->i32{
+        let mut result = 0;
+        let mut prev = 0;
+        for c in s.chars(){
+            let curr = match c{
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => 0,
+            };
+            result += curr;
+            if curr > prev{
+                result -= 2 * prev;
+            }
+            prev = curr;
+        }
+        result
+    }
+}
