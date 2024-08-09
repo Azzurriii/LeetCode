fc04df55738147308e29c3263915b74ac7b1295c
Võ Tuấn Thành <147973915+Azzurriii@users.noreply.github.com>
2024-08-09 09:51:44 +0700

    Time: 1 ms (84.50%) | Memory: 2.5 MB (10.70%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.rs
@@ -0,0 +1,29 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut prev = None;
+        let mut current = head;
+        while let Some(mut boxed_node) = current.take() {
+            let next = boxed_node.next.take();
+            boxed_node.next = prev.take();
+            prev = Some(boxed_node);
+            current = next;
+        }
+        prev
+    }
+}
