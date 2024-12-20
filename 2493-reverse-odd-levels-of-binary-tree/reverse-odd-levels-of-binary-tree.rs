09eb83ccdcde29c916c3f4233c2f08c0504ed941
Vo Tuan Thanh <147973915+Azzurriii@users.noreply.github.com>
2024-12-20 09:23:45 +0700

    Time: 0 ms (100.00%) | Memory: 3.7 MB (50.00%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.rs
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.rs
@@ -20,28 +20,38 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn reverse_odd_levels(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-		let mut a = vec![root.as_ref().unwrap().clone()];
-		let mut level = 0;
-		while !a.is_empty() {
-			let b = a;
-			a = vec![];
-			for node in &b {
-				if node.borrow().left.is_some() {
-					a.push(node.borrow().left.as_ref().unwrap().clone());
-					a.push(node.borrow().right.as_ref().unwrap().clone());
-				}
-			}
-			level += 1;
-			if level % 2 != 0 && a.len() > 0 {
-				let (mut i, mut j) = (0, a.len() - 1);
-				while i < j {
-					let tmp = a[i].borrow().val;
-					a[i].borrow_mut().val = a[j].borrow().val;
-					a[j].borrow_mut().val = tmp;
-					i += 1; j -= 1;
-				}
-			}
-		}
-		root
-	}
+        let mut current_level = vec![root.as_ref().unwrap().clone()];
+        let mut level = 0;
+        
+        while !current_level.is_empty() {
+            let mut next_level = vec![];
+            for node in &current_level {
+                let node_borrow = node.borrow();
+                if let Some(left) = &node_borrow.left {
+                    next_level.push(left.clone());
+                }
+                if let Some(right) = &node_borrow.right {
+                    next_level.push(right.clone());
+                }
+            }
+            
+            level += 1;
+            if level % 2 != 0 && !next_level.is_empty() {
+                let mut i = 0;
+                let mut j = next_level.len() - 1;
+                while i < j {
+                    let left_val = next_level[i].borrow().val;
+                    let right_val = next_level[j].borrow().val;
+                    next_level[i].borrow_mut().val = right_val;
+                    next_level[j].borrow_mut().val = left_val;
+                    i += 1;
+                    j -= 1;
+                }
+            }
+            
+            current_level = next_level;
+        }
+        
+        root
+    }
 }
